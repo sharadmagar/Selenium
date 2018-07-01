8095fc3a89c066ba85ff9932b8fda03d97c692e6
SHARAD\Dell <sharadmagar450@gmail.com>
2018-07-01 20:48:51 +0530

    Added the New Functions in Common Functions.java

--- Common_Functions.java
+++ Common_Functions.java
@@ -1,13 +1,38 @@
 package ForAppTesting;
+import java.util.List;
+
 import org.openqa.selenium.By;
 //import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Common_Functions {
 
-	public static WebDriver driver = new FirefoxDriver();
+	private static WebDriver GetDriver(String Browser)
+	{
+		if(Browser=="FireFox")
+		{
+			WebDriver DriverFireFox = new FirefoxDriver();
+			return DriverFireFox;
+		}
+		else if(Browser=="Chrome")
+		{
+			WebDriver DriverChrome = new ChromeDriver();
+			return DriverChrome;
+		}
+		else
+		{
+			WebDriver DriverExplorer = new InternetExplorerDriver();
+			return DriverExplorer;
+		}
+		
+	}
+	
+	private static WebDriver driver = GetDriver("FireFox");
 	
 	public static WebElement FindElementBySingleTag(String FindElementBy, String ElementVal)
 	{
@@ -108,9 +133,145 @@ public class Common_Functions {
 		Element.sendKeys(Key);
 	}
 	
+	public static String ClickRadioChkBtn(String FindElementBy,String ElementVal)
+	{
+		WebElement Element = FindElementBySingleTag(FindElementBy,ElementVal);
+		if(Element==null)
+		{
+			return "False, WebElement not found";
+		}
+		else
+		{
+			Element.click();
+			return "True, Clicked on Btn "+ ElementVal;
+		}
+	}
+	
+	public static String SelectDropDownItem(String FindElementBy,String ElementVal, String Value, String SelectType)
+	{
+		WebElement Element = FindElementBySingleTag(FindElementBy,ElementVal);
+		if(Element==null)
+		{
+			return "False, WebElement not found";
+		}
+		else
+		{
+			Select DropListItem = new Select(Element);
+			if(SelectType=="Text")
+			{
+				DropListItem.selectByVisibleText(Value);
+			}
+			else if(SelectType=="Index")
+			{
+				int val = Integer.parseInt(Value);
+				DropListItem.selectByIndex(val);
+			}
+			else if(SelectType=="Value")
+			{
+				DropListItem.selectByValue(Value);
+			}
+			
+			return "True, Item Selected from list is "+ Value;
+		}		
+	}
+	
+	public static String DeselectDropDownItem(String FindElementBy,String ElementVal, String Value, String DeselectType)
+	{
+		WebElement Element = FindElementBySingleTag(FindElementBy,ElementVal);
+		if(Element==null)
+		{
+			return "False, WebElement not found";
+		}
+		else
+		{
+			Select DropListItem = new Select(Element);
+			if(DeselectType=="Text")
+			{
+				DropListItem.deselectByVisibleText(Value);
+			}
+			else if(DeselectType=="Index")
+			{
+				int val = Integer.parseInt(Value);
+				DropListItem.deselectByIndex(val);
+			}
+			else if(DeselectType=="Value")
+			{
+				DropListItem.deselectByValue(Value);
+			}
+			else
+			{
+				DropListItem.deselectAll();
+			}
+			
+			return "True, Item Deselected from list is "+ Value;
+		}		
+	}
+	
+	public static String GetTableVal(String FindElementBy, String ElementVal, String UniqueColumnName, String UniqueVal, String ColumnName)
+	{
+		int ColIndex = GetColumnIndexByVal(FindElementBy, ElementVal, ColumnName);
+		
+		int RowIndex = GetRowIndexByVal(FindElementBy, ElementVal, UniqueColumnName, UniqueVal);
+		 
+		String ActualVal = ElementVal+"/tr["+RowIndex+"]/td["+ColIndex+"]";
+		WebElement Cell =  FindElementBySingleTag(FindElementBy,ActualVal);
+		String Value = Cell.getText();
+		if(Value==null || Value=="")
+		{
+			return null;
+		}
+		else
+		{
+			return Value;
+		}
+	}
+	
+	public static int GetColumnIndexByVal(String FindElementBy, String ElementVal, String ColumnName)
+	{
+		String NewElementVal = ElementVal+"/tr[1]/th";
+		
+		@SuppressWarnings("unchecked")
+		List<WebElement> ColCount = (List<WebElement>) FindElementBySingleTag(FindElementBy,NewElementVal);
+		int Ccount = ColCount.size();
+		for(int j=0;j<Ccount;j++)
+		{
+			String NewVal = ElementVal+"/tr[1]/th["+j+"]";
+			WebElement Cell =  FindElementBySingleTag(FindElementBy,NewVal);
+			String Value = Cell.getText();
+			if(ColumnName.equals(Value))
+				return j;
+			else
+				return -1;
+		}
+		return -1;
+	}
+	
+	
+	public static int GetRowIndexByVal(String FindElementBy, String ElementVal, String ColumnName, String ColumnVal)
+	{
+		String NewElementVal = ElementVal+"/tr";
+		
+		@SuppressWarnings("unchecked")
+		List<WebElement> RowCount = (List<WebElement>) FindElementBySingleTag(FindElementBy,NewElementVal);
+		int Rcount = RowCount.size();
+		
+		int Colindedx = GetColumnIndexByVal(FindElementBy, ElementVal, ColumnName);
+		for(int j=0;j<Rcount;j++)
+		{
+			String NewVal = ElementVal+"/tr["+j+"1]/th["+Colindedx+"]";
+			WebElement Cell =  FindElementBySingleTag(FindElementBy,NewVal);
+			String Value = Cell.getText();
+			if(NewElementVal.equals(Value))
+				return j;
+			else
+				return -1;
+		}
+		return -1;
+	}
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
